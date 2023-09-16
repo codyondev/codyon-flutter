1270edbfe296fbfd9d9b0add8f6e4fbcfc1cb2af
northfacegawd <chuck5732590@gmail.com>
2023-09-16 14:07:52 +0900

    feat: 폰트 및 컬러 설정

---
+++ lib/common/constants/colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+const PRIMARY_COLOR = Color(0x004400eb);
+const SECONDARY_COLOR = Color(0x00ffffff);
+const BACKGROUND_COLOR = Color(0x00ffffff);
+const SUFACE_COLOR = Color(0x00ffffff);
+const ERROR_COLOR = Color(0x00f62f32);
+const SUCCESS_COLOR = Color(0x0053D561);
+const WARNING_COLOR = Color(0x00FAB748);
+const ON_PRIMARY_COLOR = Color(0x000C0C0C);
+const ON_SECONDARY_COLOR = Color(0x000C0C0C);
+const ON_BACKGROUND_COLOR = Color(0x000C0C0C);
+const ON_SUFACE_OLOR = Color(0x000C0C0C);
+
+const WHITE = Color(0x00FFFFFF);
+const GRAY_50 = Color(0x00F7F8F9);
+const GRAY_100 = Color(0x00EBEBED);
+const GRAY_200 = Color(0x00C9CDD2);
+const GRAY_300 = Color(0x009EA4AA);
+const GRAY_400 = Color(0x0072787F);
+const GRAY_500 = Color(0x00454C53);
+const GRAY_600 = Color(0x0026282B);
+const GRAY_700 = Color(0x001B1D1F);
+const BLACK = Color(0x000C0C0C);
