3ac066a45ae21f54a221342af9e4c664bac1df23
northfacegawd <chuck5732590@gmail.com>
2023-10-08 01:49:42 +0900

    feat: welcome screen 생성

---
+++ lib/auth/screens/welcome_screen.dart
@@ -0,0 +1,13 @@
+import 'package:codyon/common/layout/default_layout.dart';
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const DefaultLayout(
+      child: Column(),
+    );
+  }
+}
--- lib/router.dart
+++ lib/router.dart
@@ -1,4 +1,5 @@
-import 'package:codyon/auth/screens/LoginScreen.dart';
+import 'package:codyon/auth/screens/login_screen.dart';
+import 'package:codyon/auth/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -7,18 +8,24 @@ final _rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
 
 enum Routes {
   login,
+  welcome,
 }
 
 final routerProvider = Provider<GoRouter>(
   (ref) {
     return GoRouter(
       navigatorKey: _rootNavigatorKey,
-      initialLocation: "/login",
+      initialLocation: "/welcome",
       routes: [
         GoRoute(
           path: "/login",
           name: Routes.login.name,
           builder: (context, state) => const LoginScreen(),
+        ),
+        GoRoute(
+          path: "/welcome",
+          name: Routes.welcome.name,
+          builder: (context, state) => const WelcomeScreen(),
         )
       ],
     );
