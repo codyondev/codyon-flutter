c931fdace6266b469d793e18a0dbda518badd1ee
northfacegawd <chuck5732590@gmail.com>
2023-10-09 01:05:15 +0900

    fix: welcome screen expanded 제거

--- lib/auth/screens/welcome_screen.dart
+++ lib/auth/screens/welcome_screen.dart
@@ -15,62 +15,60 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       child: Center(
-        child: Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "원하는 스타일의 완벽한 발견",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w500,
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "원하는 스타일의 완벽한 발견",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const SizedBox(height: 40),
+              Hero(
+                tag: "codyon_logo",
+                child: SvgPicture.asset("assets/icons/logo_text.svg"),
+              ),
+              const SizedBox(height: 52),
+              _SnsLoginButton(
+                "카카오 계정으로 로그인",
+                foregroundColor: Colors.black.withOpacity(0.85),
+                backgroundColor: const Color(0xFFFEE500),
+                svgIcon: SvgPicture.asset("assets/icons/kakao_symbol.svg"),
+              ),
+              const SizedBox(height: 16),
+              _SnsLoginButton(
+                "카카오 계정으로 로그인",
+                foregroundColor: Colors.white,
+                backgroundColor: const Color(0xFF03C75A),
+                svgIcon: SvgPicture.asset("assets/icons/naver_symbol.svg"),
+              ),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SocialLoginBadge(
+                    provider: LoginProvider.google,
+                    onPressed: (provider) {},
                   ),
-                ),
-                const SizedBox(height: 40),
-                Hero(
-                  tag: "codyon_logo",
-                  child: SvgPicture.asset("assets/icons/logo_text.svg"),
-                ),
-                const SizedBox(height: 52),
-                _SnsLoginButton(
-                  "카카오 계정으로 로그인",
-                  foregroundColor: Colors.black.withOpacity(0.85),
-                  backgroundColor: const Color(0xFFFEE500),
-                  svgIcon: SvgPicture.asset("assets/icons/kakao_symbol.svg"),
-                ),
-                const SizedBox(height: 16),
-                _SnsLoginButton(
-                  "카카오 계정으로 로그인",
-                  foregroundColor: Colors.white,
-                  backgroundColor: const Color(0xFF03C75A),
-                  svgIcon: SvgPicture.asset("assets/icons/naver_symbol.svg"),
-                ),
-                const SizedBox(height: 16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SocialLoginBadge(
-                      provider: LoginProvider.google,
-                      onPressed: (provider) {},
-                    ),
-                    SocialLoginBadge(
-                      provider: LoginProvider.facebook,
-                      onPressed: (provider) {},
-                    ),
-                    SocialLoginBadge(
-                      provider: LoginProvider.apple,
-                      onPressed: (provider) {},
-                    ),
-                  ].withSpaceBetween(width: 32),
-                ),
-                const SizedBox(height: 40),
-                _GoEmailLoginButton(
-                  onPressed: () => context.pushNamed(Routes.login.name),
-                ),
-              ],
-            ),
+                  SocialLoginBadge(
+                    provider: LoginProvider.facebook,
+                    onPressed: (provider) {},
+                  ),
+                  SocialLoginBadge(
+                    provider: LoginProvider.apple,
+                    onPressed: (provider) {},
+                  ),
+                ].withSpaceBetween(width: 32),
+              ),
+              const SizedBox(height: 40),
+              _GoEmailLoginButton(
+                onPressed: () => context.pushNamed(Routes.login.name),
+              ),
+            ],
           ),
         ),
       ),
