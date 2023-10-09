581f99dc143b24f66bc368b9e67eb303b5a4e918
northfacegawd <chuck5732590@gmail.com>
2023-10-09 13:36:08 +0900

    feat: home app bar 구현

---
+++ lib/home/widgets/home_header.dart
@@ -0,0 +1,41 @@
+import 'package:codyon/extensions.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class HomeHeader extends StatelessWidget {
+  const HomeHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        left: 16,
+        right: 16,
+        top: MediaQuery.of(context).padding.top + 8,
+        bottom: 8,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SvgPicture.asset("assets/icons/logo_text.svg"),
+          Row(
+            children: <Widget>[
+              InkWell(
+                onTap: () {},
+                child: SvgPicture.asset("assets/icons/fi_search.svg"),
+              ),
+              InkWell(
+                onTap: () {},
+                child: SvgPicture.asset("assets/icons/fi_bell.svg"),
+              ),
+              InkWell(
+                onTap: () {},
+                child: SvgPicture.asset("assets/icons/fi_user.svg"),
+              ),
+            ].withSpaceBetween(width: 12),
+          ),
+        ],
+      ),
+    );
+  }
+}
